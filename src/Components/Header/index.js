84053c1254ee121f656a75a18ebf1d69f8f97f96
GabrielSantos <gabrielsantos@GabrielSantoss-MacBook-Pro.local>
2023-05-06 16:50:04 -0300

    Updates

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -1,9 +1,16 @@
 import React from 'react';
 
-import { AppBar, Toolbar, Typography, IconButton, Avatar } from '@mui/material';
+import { AppBar, Avatar } from '@mui/material';
 import { Menu as MenuIcon } from '@mui/icons-material';
 
-import { AvatarBackground, AvatarWrapper, Logo, Wrapper } from './styles';
+import {
+  AvatarBackground,
+  AvatarWrapper,
+  Logo,
+  ToolbarWrapper,
+  IconButtonWrapper,
+  TypographyWrapper,
+} from './styles';
 
 import { useDrawer } from '../../Providers/DrawerProvider';
 
@@ -12,21 +19,19 @@ const Header = () => {
 
   return (
     <AppBar position="fixed" sx={{ background: 'linear-gradient(to right, #662d8c, #ed1e79)' }}>
-      <Toolbar>
-        <Wrapper>
-          <IconButton edge="start" color="inherit" aria-label="menu" onClick={toggleDrawer}>
-            <MenuIcon fontSize="large" />
-          </IconButton>
-          <Logo src="/logo.png" alt="Logo" />
-          <Typography variant="h6" sx={{ color: 'white', fontWeight: 'bold', flexGrow: 1 }}>
-            Dashboard
-          </Typography>
-          <AvatarWrapper>
-            <Avatar alt="Avatar Logo" src="/avatar.png" sx={{ width: 32, height: 32 }} />
-            <AvatarBackground />
-          </AvatarWrapper>
-        </Wrapper>
-      </Toolbar>
+      <ToolbarWrapper>
+        <IconButtonWrapper edge="start" color="inherit" aria-label="menu" onClick={toggleDrawer}>
+          <MenuIcon fontSize="large" />
+        </IconButtonWrapper>
+        <Logo src="/logo.png" alt="Logo" />
+        <TypographyWrapper variant="h6" sx={{ color: 'white', fontWeight: 'bold', flexGrow: 1 }}>
+          Dashboard
+        </TypographyWrapper>
+        <AvatarWrapper>
+          <Avatar alt="Avatar Logo" src="/avatar.png" sx={{ width: 32, height: 32 }} />
+          <AvatarBackground />
+        </AvatarWrapper>
+      </ToolbarWrapper>
     </AppBar>
   );
 };
--- src/Components/Header/styles.js
+++ src/Components/Header/styles.js
@@ -1,26 +1,36 @@
-import styled from 'styled-components';
+import { styled } from '@mui/system';
+import { Toolbar, IconButton, Typography } from '@mui/material';
 
-export const Wrapper = styled.div`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  width: 100%;
-`;
+export const ToolbarWrapper = styled(Toolbar)({
+  display: 'flex',
+  justifyContent: 'space-between',
+});
 
-export const Logo = styled.img`
-  height: 32px;
-`;
+export const IconButtonWrapper = styled(IconButton)({
+  marginRight: '16px',
+});
 
-export const AvatarWrapper = styled.div`
-  position: relative;
-`;
+export const Logo = styled('img')({
+  width: '50%',
+  maxWidth: 48,
+  height: 'auto',
+});
 
-export const AvatarBackground = styled.div`
-  position: absolute;
-  top: -2px;
-  right: -2px;
-  width: 36px;
-  height: 36px;
-  border-radius: 50%;
-  background-color: #fff;
-`;
+export const TypographyWrapper = styled(Typography)({
+  flexGrow: 1,
+});
+
+export const AvatarWrapper = styled('div')({
+  position: 'relative',
+});
+
+export const AvatarBackground = styled('div')({
+  position: 'absolute',
+  top: 0,
+  left: 0,
+  width: '100%',
+  height: '100%',
+  borderRadius: '50%',
+  backgroundColor: 'rgba(255, 255, 255, 0.4)',
+  zIndex: -1,
+});
